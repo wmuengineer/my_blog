05e1bcdb7e1152c5adf7100c3d26f951fbb266fb
wmuengineer <adam.d.bell@icloud.com>
2016-11-13 00:37:06 -0800

    adding master build to make to run every codeship deploy build on master to give cf plenty of deploys to practice on

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,65 @@
 #!/usr/bin/env bash
-env
-#CF Zero Downtime Deployment Script
-echo "Starting"
-echo "Download Cli"
+# Application name, please set
+APPLICATION_NAME='adam_blog' # Use hyphens - only as symbols, no _ underscores (NIRD convention)
+# Colors for cosmetics
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+ACTUAL_BRANCH=${$CI_BRANCH}              # Obtain actual branch if we are building under CODESHIP
+if [ "$ACTUAL_BRANCH" != "staging" -a "$ACTUAL_BRANCH" != "production" ] ; then
+echo -e "${YELLOW}Not deploying the branch $ACTUAL_BRANCH."
+exit 0;
+fi
+function rollback {
+cf delete $APPLICATION_NAME -f
+cf rename ${APPLICATION_NAME}-old $APPLICATION_NAME
+}
+echo -e "${YELLOW}Starting deployment of $ACTUAL_BRANCH"
+######################
+# CLI SETUP
+######################
+echo -e "${GREEN}[+] Download Cli"
 cd ../
 wget -qO cf-linux-amd64.tgz 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' && \
-tar -zxvf cf-linux-amd64.tgz && \
+tar -xf cf-linux-amd64.tgz && \
 rm cf-linux-amd64.tgz
 export PATH=$PATH:$PWD
-echo "CF Login"
+######################
+# CF LOGIN
+######################
+echo -e "${GREEN}[+] CF Login"
 cf api "$CF_API_URL"
-cf login -u "$CF_USER" -p "$CF_PASS" -o "$CF_ORG" -s "$CI_BRANCH"
-echo "Start Deploy"
-cd /home/rof/src/github.com/wmuengineer/my_blog/
+cf login -u "$CF_USER" -p "$CF_PASS" -o "$CF_ORG" -s "$ACTUAL_BRANCH"
+######################
+# APP BACKUP AND PUSH
+######################
+echo -e "${GREEN}[+] Pushing application"
+cd - # Go back to the application's folder
 rm -rf vendor/bundle
-cf rename adam_blog adam_blog-old 
-cf push adam_blog -f ./"$CI_BRANCH"-manifest.yml --no-start
-cf start adam_blog
-cf delete adam_blog-old -f
+cf rename $APPLICATION_NAME ${APPLICATION_NAME}-old
+if ! cf push $APPLICATION_NAME -f ./${ACTUAL_BRANCH}-manifest.yml --no-start; then
+echo -e "${RED}[-] Application could not be pushed, rolling back..."
+rollback
+exit -1;
+fi
+######################
+# APP ENV SETUP
+######################
+echo -e "${GREEN}[+] Setting application environment"
+######################
+# APP START
+######################
+echo -e "${GREEN}[+] Starting application"
+if ! cf start $APPLICATION_NAME; then
+echo -e "${RED}[-] Application could not be started, rolling back..."
+rollback
+exit -1;
+fi
+######################
+# CLEANUP
+######################
+cf delete ${APPLICATION_NAME}-old -f
 cf logout
-echo "Deploy Complete"
+echo -e "${GREEN}[+] Deploy Complete"
+exit 0
