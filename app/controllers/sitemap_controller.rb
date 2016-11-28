8bf7bf85c1daf35fc8b66230ed2c09170e1ca3ad
wmuengineer <adam.d.bell@icloud.com>
2016-11-27 21:37:14 -0800

    Revised controller logic and removed misnamed file.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,11 +2,11 @@ class SitemapController < ApplicationController
   layout nil
   def index
     headers['Content-Type'] = 'application/xml'
-    # respond_to do |format|
-    #   format.xml do
-    #     @posts = Monologue::Post.published
-    #     @default_last_mod = @posts.pluck(:updated_at).first
-    #   end
-    # end
+    respond_to do |format|
+      format.xml do
+        @posts = Monologue::Post.published
+        @default_last_mod = @posts.pluck(:updated_at).first
+      end
+    end
   end
 end
