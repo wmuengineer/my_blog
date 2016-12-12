92da7485903bfcd01ee4462db2861e84f597722c
wmuengineer <adam.d.bell@icloud.com>
2016-12-11 20:27:08 -0800

    test are now working for sitemap

--- spec/features/sitemaps_spec.rb
+++ spec/features/sitemaps_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Sitemaps", type: :feature do
-    it "has a sitemap" do
-      visit '/sitemap.xml'
-      expect(page).to have_content 'jibberish-1'
-    end
-
-
+  it "has a sitemap" do
+    post = create(:monologue_post)
+    visit '/sitemap.xml'
+    expect(page).to have_content 'jibber-1'
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,14 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.before(:suite) do
+   DatabaseCleaner.strategy = :transaction
+   DatabaseCleaner.clean_with(:truncation)
+ end
+
+ config.around(:each) do |example|
+   DatabaseCleaner.cleaning do
+     example.run
+   end
+ end
 end
